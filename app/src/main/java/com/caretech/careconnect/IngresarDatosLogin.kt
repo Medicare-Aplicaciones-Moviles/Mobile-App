facd081a68df1320170e53240f5331f3a1c4a7c9
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2024-04-24 21:31:35 -0500

    First commit

---
+++ app/src/main/java/com/caretech/careconnect/IngresarDatosLogin.kt
@@ -0,0 +1,36 @@
+package com.caretech.careconnect
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class IngresarDatosLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ingresar_datos_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Boton back
+        val btnBack = findViewById<ImageButton>(R.id.ibBack)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, RolCuentaActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/caretech/careconnect/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.caretech.careconnect
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        //Boton login
+        val btnLogin = findViewById<Button>(R.id.btLogIn)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, RolCuentaActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+
+
+
+}
