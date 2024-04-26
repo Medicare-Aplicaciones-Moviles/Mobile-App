3f8586f372f35656a38c01762309afd46ae60219
CountryGGG <133285281+CountryGGG@users.noreply.github.com>
2024-04-25 23:17:15 -0500

    feat(main): add Register 1/2
    Añadí diferentes activities para completar el Register

--- app/src/main/java/com/caretech/careconnect/IngresarDatosRegistro.kt
+++ app/src/main/java/com/caretech/careconnect/IngresarDatosRegistro.kt
@@ -2,45 +2,41 @@ package com.caretech.careconnect
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
-class MainActivity : AppCompatActivity() {
+class IngresarDatosRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ingresar_datos_registro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //Boton Sign Up
-        val btnSingUp = findViewById<Button>(R.id.btSignUp)
+        //Boton back
+        val btnBack = findViewById<ImageButton>(R.id.ibBack)
 
-        btnSingUp.setOnClickListener {
-            val intent = Intent(this, RolInteractivityRegister::class.java)
-            startActivity(intent)
-        }
-
-        //Boton login
-        val btnLogin = findViewById<Button>(R.id.btLogIn)
-
-        btnLogin.setOnClickListener {
+        btnBack.setOnClickListener {
             val intent = Intent(this, RolCuentaActivity::class.java)
             startActivity(intent)
         }
 
+        val tvLogin = findViewById<TextView>(R.id.TvredirLog_in)
 
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
-
-
-
-
 }
