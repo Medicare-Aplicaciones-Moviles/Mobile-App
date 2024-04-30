9fd4977417bb545ed40d3cd935b4e0efe993fb0b
AngieV <89101151+AngieValverdeSalazar@users.noreply.github.com>
2024-04-30 17:47:51 -0500

    Update HistorialMedicoResumenActivity.kt

--- app/src/main/java/com/caretech/careconnect/HistorialMedicoResumenActivity.kt
+++ app/src/main/java/com/caretech/careconnect/HistorialMedicoResumenActivity.kt
@@ -3,10 +3,7 @@ package com.caretech.careconnect
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.caretech.careconnect.Historial.Alergia
