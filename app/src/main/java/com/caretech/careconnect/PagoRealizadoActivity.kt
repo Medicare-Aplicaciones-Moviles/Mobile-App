b62bc565244d85f3431335e446b95d890dfb8944
Paolo Sebastián Checa Apolinario <u202112749@upc.edu.pe>
2024-05-25 00:31:29 -0500

    feature: pago cita médica

--- app/src/main/java/com/caretech/careconnect/PagoRealizadoActivity.kt
+++ app/src/main/java/com/caretech/careconnect/PagoRealizadoActivity.kt
@@ -2,31 +2,28 @@ package com.caretech.careconnect
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CitaMenuActivity : AppCompatActivity() {
+class PagoRealizadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cita_menu)
+        setContentView(R.layout.activity_pago_realizado)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //Iv sacar cita
-        val ivSacarCita = findViewById<ImageView>(R.id.ivSacarCitaButton)
+        val btnFinalizar = findViewById<Button>(R.id.btFinalizar)
 
-        ivSacarCita.setOnClickListener {
-            val intent = Intent(this, SeleccionarFechaActivity::class.java)
+        btnFinalizar.setOnClickListener {
+            val intent = Intent(this, PatientMenuActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 }
--- app/src/main/java/com/caretech/careconnect/SeleccionarFechaActivity.kt
+++ app/src/main/java/com/caretech/careconnect/SeleccionarFechaActivity.kt
@@ -2,28 +2,27 @@ package com.caretech.careconnect
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CitaMenuActivity : AppCompatActivity() {
+class SeleccionarFechaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cita_menu)
+        setContentView(R.layout.activity_seleccionar_fecha)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //Iv sacar cita
-        val ivSacarCita = findViewById<ImageView>(R.id.ivSacarCitaButton)
+        val btnSiguiente = findViewById<Button>(R.id.btSiguiente)
 
-        ivSacarCita.setOnClickListener {
-            val intent = Intent(this, SeleccionarFechaActivity::class.java)
+        btnSiguiente.setOnClickListener {
+            val intent = Intent(this, PagoCitaActivity::class.java)
             startActivity(intent)
         }
 
