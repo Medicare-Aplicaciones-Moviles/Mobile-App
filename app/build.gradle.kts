3883611bd0c7dc34a1b470a0c497d5a372d761e3
CountryGGG <133285281+CountryGGG@users.noreply.github.com>
2024-05-28 01:03:00 -0500

    feat: add activity ingresos pagos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,17 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.material.icons.extended)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
